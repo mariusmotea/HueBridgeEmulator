33ef8f458cc00bddd526d22e39f3d17cae981618
Janusz Korczak <korczak_ja_ext@gigaset.com>
2022-04-15 18:16:58 +0200

    fix rule handler access to global variable bridge_config

--- HueBridgeEmulator.py
+++ HueBridgeEmulator.py
@@ -115,6 +115,7 @@ def scheduler_processor():
 
 
 def process_rules():
+    global bridge_config
     for schedule in bridge_config["schedules"].keys():
         if bridge_config["schedules"][schedule]["status"] == "enabled":
             if bridge_config["schedules"][schedule]["localtime"].startswith("W"):
